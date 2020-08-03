75a9e9de846e5d0af0e36f01502cd720a0f7def9
Ivy Bean <ivieking@gmail.com>
2020-08-03 12:21:46 -0400

    added cards for 5-day forecast

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#search-button").on("click", function() {
     var searchValue = $("#search-value").val();
-
+    console.log(searchValue);
     // clear input box
 
     searchWeather(searchValue);
@@ -18,8 +18,8 @@ $(document).ready(function() {
 
   function searchWeather(searchValue) {
     $.ajax({
-      type: "",
-      url: "" + searchValue + "",
+      type: "GET",
+      url: "api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=69512b2524e83d5bf183c5680485a288",
       dataType: "json",
       success: function(data) {
         // create history link for this search
@@ -45,8 +45,8 @@ $(document).ready(function() {
   
   function getForecast(searchValue) {
     $.ajax({
-      type: "",
-      url: "" + searchValue + "",
+      type: "GET",
+      url: "api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=69512b2524e83d5bf183c5680485a288",
       dataType: "json",
       success: function(data) {
         // overwrite any existing content with title and empty row
@@ -92,3 +92,5 @@ $(document).ready(function() {
     makeRow(history[i]);
   }
 });
+
+
